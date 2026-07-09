186dea220ebc7f70dcc1cb870fdd97a3f86c0d69
Ahmar Ansari <ahmaransari29@gmail.com>
2026-07-09 17:29:31 +0530

    hide map key from app.json

---
+++ furrcircle-expo/app.config.js
@@ -0,0 +1,14 @@
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY || ""
+        }
+      }
+    }
+  };
+};
